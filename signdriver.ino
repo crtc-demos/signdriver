369560656edd6fd7de45cbf15781b9d3caa12819
Simon Rawles <github@rawles.net>
2014-04-26 23:42:55 +0100

    Removal of dead code. Tidy up.

--- signdriver.ino
+++ signdriver.ino
@@ -1,39 +1,27 @@
+/*   oo   oo  * signdriver
+ *  o    o    *
+ *  ooo  o    * Arduino code to drive a 128x16 LED banner display
+ *            *
+ *  ooo   oo  *
+ *   o   o    *
+ *   o   ooo  */
+
 #include  <SPI.h>
 
-unsigned short redgrid[64];
-unsigned short greengrid[64];
-
-#define PIN_S 13
-#define PIN_R1 11
-#define PIN_EN 3
 #define PIN_L 2
-
-#define PIN_A 7
-#define PIN_B 6
-#define PIN_C 5
+#define PIN_EN 3
 #define PIN_D 4
-
-#define BOARD_LED 13
+#define PIN_C 5
+#define PIN_B 6
+#define PIN_A 7
+#define PIN_R1 11
+#define PIN_S 13
 
 #define DELAY_MS 2
 #define DELAY_SCAN 1
-
-void blink() { 
-  pinMode(BOARD_LED, OUTPUT);
-  for ( int i = 0; i < 5; i++ ) { 
-    digitalWrite(BOARD_LED, HIGH);
-    delay(1000);
-    digitalWrite(BOARD_LED, LOW);
-    delay(1000);
-  }
-  
-  
-}
+#define OFFCYCLES 7
 
 void setup() {
-  bool redcolortable[5] = { true, true, false, false, false };
-  bool greencolortable[5] = { false, true, true, false, false };
-  
   //pinMode(PIN_S, OUTPUT);
   //pinMode(PIN_R1, OUTPUT);
   pinMode(PIN_EN, OUTPUT);
@@ -42,22 +30,7 @@ void setup() {
   pinMode(PIN_B, OUTPUT);
   pinMode(PIN_C, OUTPUT);
   pinMode(PIN_D, OUTPUT);
-  
-  //blink();
-  
-  for (int y = 0; y < 16; y++)
-    redgrid[y] = greengrid[y] = 0;
-  
-  for ( int y = 0; y < 16; y++ ) {
-    for ( int x = 0; x < 64; x++ ) {
-      int color = ( ( x + y ) % 5 );
-      
-      redgrid[x] |= redcolortable[color] << y;
-      greengrid[x] |= greencolortable[color] << y;
-
-    }
-  }  
-  
+    
   digitalWrite(PIN_L, LOW);
   digitalWrite(PIN_S, HIGH);
 
@@ -83,7 +56,11 @@ static int power_switch = 0x00;
 
 int decodechar (char c)
 {
-  return (c >= '0' && c <= '9') ? c - '0' : (c >= 'A' && c <= 'F') ? c - 'A' + 10 : -1;
+  return
+    (c >= '0' && c <= '9') ? c - '0' :
+    (c >= 'A' && c <= 'F') ? c - 'A' + 10 : 
+    (c >= 'a' && c <= 'f') ? c - 'a' + 10 : 
+    -1;
 }
 
 void loop() {
@@ -91,12 +68,13 @@ void loop() {
   while (Serial.available() > 0)
   {
     int x, incomingByte = Serial.read();
-    if (incomingByte == '\r' || incomingByte == '\n' || incomingByte == 'X' )
+    if ( incomingByte == 'R' || incomingByte == 'r' )
     {
       cur_screen = 0;
       cur_row = 0;
       cur_nybble = 0;
       cur_byte = 0;
+      power_switch = 0x00;
       continue;
     }
     x = decodechar (incomingByte);
@@ -128,7 +106,7 @@ void loop() {
     }
   }
   
-  disp_screen = (disp_screen+1) & 7;
+  disp_screen = (disp_screen+1) & OFFCYCLES;
   for ( int y = 0; y < 16  ; y++ )
   {
     for (int byt = 0; byt < 32; byt++)
